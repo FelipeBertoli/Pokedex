94931694598365a2a852eb624d1d03434820d066
Felipe Bertoli <103579021+FelipeBertoli@users.noreply.github.com>
2023-08-17 21:24:32 -0300

    Adicionando elementos dinâmicos ao HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,33 @@
 
-const offset = 0
-const limit = 26
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.log(error))
-    .finally(() => console.log('Requisição concluída'))
+function convert(pokemon) {
+    return `
+            <li class="pokemon">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="details">
+                    <ol class="types">
+                        <li class="type">Grass</li>
+                        <li class="type">Poison</li>
+                    </ol>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                        alt="${pokemon.name}">
+                </div>
+            </li>
+        `
+}
+
+
+const pokemonList = document.getElementById('pokemonList')
+
+
+pokeApi.getPokemon.then((pokemons) => { 
+    const listItems = []
+
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i]
+        listItems.push(convert(pokemon))
+
+        pokemonList.innerHTML = listItems
+    }
+})
